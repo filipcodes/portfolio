a9b2fb06ab0ad06e5a286fc6d48f175c4442b313
SIPOS Filip SK79A <filip.sipos@student.leaf.academy>
2024-07-28 11:50:32 +0200

    i18n imported and instance created

--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
 import { createApp } from "vue";
+import { createI18n } from "vue-i18n";
+
 import App from "./App.vue";
+
+export const i18n = createI18n({
+  locale: "en",
+  fallbackLocale: "en",
+  messages: {
+    //! ENGLISH
+    en: {
+      navigation: {
+        contactMeButton: "Get in touch",
+      },
+
+      intro: {
+        description:
+          "My name is Filip, and I am a Front-end developer based in Slovakia. I am passionate about creating smooth, engaging, and appealing digital experiences. ",
+        socialLinkDescriptions: {
+          github: "Check out my GitHub",
+          linkedin: "Check out my LinkedIn",
+          cv: "Download my CV",
+        },
+      },
+
+      //! SLOVAK
+      sk: {},
+    },
+  },
+});
 
 createApp(App).mount("#app");
